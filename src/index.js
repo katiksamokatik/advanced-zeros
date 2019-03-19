0920c294abe99e1761c9ce407cdfe2ec9e688017
Katya <kat.lapitskaya@gmail.com>
2019-03-19 17:15:12 +0300

    feature: create function which counts zeros of a factorial number, 98% test :(

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let arr = [];
+  let counter = 0;
+
+    if (base > 0 && base != 1) {
+      for (let i = 2; i <= base; i++) {
+        while (base % i == 0 && base > 1) {
+          arr.push(i);
+          base = base / i;
+        }
+          if(base / i == 1){
+           arr.push(base);
+          }
+      }
+    } else if(base == 1){
+      return 1;
+    }
+  
+  
+let max = Math.max(...arr);
+  
+let countOfMax = 0;
+for(let k = 0; k < arr.length; ++k){
+    if(arr[k] === max)
+        countOfMax++;
+}
+  
+  for(let j = max; number/j >= 1; j *= max) {
+    counter += Math.floor(number / j);
+  }    
+  return Math.floor(counter / countOfMax);
 }
